faf3ebe8298c4a7e58d6e01cce8b752cf2fc5d54
Artem Sukhomlyn <14799785+ArtemSukhomlyn@users.noreply.github.com>
2020-09-27 09:24:49 +0300

    Change-Id: Iac5d16365d6981db8cb4c6d6dc1cfb8544b64813 Signed-off-by: Artem Sukhomlyn <14799785+ArtemSukhomlyn@users.noreply.github.com>

---
+++ Test Suites/Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0da405e5-db76-4019-b2ed-f92a4fb902b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>407afdc3-53e5-4176-ad66-12a487534c7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Not success login script (wrong login_and_passord)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>20ede43c-287c-4d05-a6a2-be761ddc8553</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Not success login script (wrong passord)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f04e9c63-a585-4c78-8aa0-b4edb378e8fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Not success login script (wrong user)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2eb8b9b-b656-470a-bd60-6cd166594e69</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Success login script</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
